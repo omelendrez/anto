9bba432cbc6f80fb6b701fca40b4b4dee891d40b
omelendrez <omar.melendrez@gmail.com>
2021-06-26 12:40:47 -0300

    Agregar paginación

--- index.js
+++ index.js
@@ -14,14 +14,27 @@ export const generarTitulos = (rows) => {
 export const generarTabla = (rows) => {
   const objeto = rows[0];
   const headers = Object.keys(objeto); // ['name',  'edad', etc.]
-  const response = [`<tr>`];
+  const response = ["<tr>"];
   rows.forEach((objeto) => {
     headers
       .filter((nombreColumna) => !ocultarColumnas.includes(nombreColumna))
       .forEach((campo) => {
         response.push(`<td>${objeto[campo]}</td>`);
       });
-    response.push(`</tr>`);
+    response.push("</tr>");
   });
   return response.join("");
+};
+
+export const loadData = (page = 1) => {
+  fetch(
+    `https://vmr.herokuapp.com/api/consultations?limit=5&page=${page}`
+  ).then((response) =>
+    response.json().then((data) => {
+      const rows = data.consultations.rows;
+      document.querySelector(".table thead").innerHTML = generarTitulos(rows);
+      document.querySelector(".table tbody").innerHTML = generarTabla(rows);
+      document.getElementById("pageNumber").innerText = page;
+    })
+  );
 };
