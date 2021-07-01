e9aac62b35bb4580bef1d194fc6347b4b9dfa355
omelendrez <omar.melendrez@gmail.com>
2021-07-01 12:15:14 -0300

    Implement model change

--- index.js
+++ index.js
@@ -26,15 +26,49 @@ export const generarTabla = (rows) => {
   return response.join("");
 };
 
-export const loadData = (page = 1) => {
+export const loadData = ({ page, records, search, model }) => {
   fetch(
-    `https://vmr.herokuapp.com/api/consultations?limit=5&page=${page}`
+    `https://vmr.herokuapp.com/api/${model}?limit=${records}&page=${page}&filter=${search}`
   ).then((response) =>
     response.json().then((data) => {
-      const rows = data.consultations.rows;
+      /**
+       * ejemplo de cómo viene la data:
+       *
+       * 
+       * customers: {
+            "count": 730,
+            "rows": [
+                {
+                    "id": 7454,
+                    "name": "MARIELA MUZZIO",
+                    "address": "Sixto laxpiur 3434",
+                    "phone": "2915119035",
+                    "email": "",
+                    "observations": "",
+                    "balance": 0,
+                    "updatedAt": "2021-06-30T22:01:19.000Z",
+                    "pets": [
+                        {
+                            "id": 9364,
+                            "name": "TEODORO",
+                            "statusId": 1
+                        }
+                    ],
+                    "user": {
+                        "name": "angie"
+                    }
+                },
+
+              ]
+      } 
+       *  */
+
+      const modelData = data[model];
+      const rows = modelData.rows;
       document.querySelector(".table thead").innerHTML = generarTitulos(rows);
       document.querySelector(".table tbody").innerHTML = generarTabla(rows);
       document.getElementById("pageNumber").innerText = page;
+      document.getElementById("totalRecords").innerText = modelData.count;
     })
   );
 };
